0598c3d8fb025a54b6c29cb3a61334210eda891a
mshagirov <muratshagirov@gmail.com>
2020-01-03 17:14:46 +0800

    trying cleaning 6

--- vertex_simulation/primitives.py
+++ vertex_simulation/primitives.py
@@ -227,8 +227,8 @@ def VoronoiRegions2Edges(regions):
     representations e.g. [j,i], but not both (i.e. no duplicate edges). Note that edge indexing in
     `cells` starts from 1 (not 0!). Thus, when reading cell edges from `edge_list` you need to
     substract 1 from (absolute value of) these indices. E.g.
-
-    - ```python
+    - Usage:
+        - ```python
     vrn = Voronoi(seeds) # <-- scipy.spatial.Voronoi
     edge_list,cells = VoronoiRegions2Edges(vrn.regions)
     edges = np.array(edge_list)
